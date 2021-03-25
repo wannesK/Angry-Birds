4b19b4e9985e93a2c8da37587d8c706707c670e1
wannesK <66689660+wannesK@users.noreply.github.com>
2021-03-25 17:59:09 +0300

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    public float releaseBallTime = .15f;
+    public float maxDragDistance = 2f;
+
+    public Rigidbody2D hookRigid;
+    public GameObject nextBall;
+
+    private bool isPressed = false;
+    private Rigidbody2D rigid;
+    private SpringJoint2D joint;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        joint = GetComponent<SpringJoint2D>();
+    }
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Vector3.Distance(mousePos, hookRigid.position) > maxDragDistance)
+            {
+                rigid.position = hookRigid.position + (mousePos - hookRigid.position).normalized * maxDragDistance;
+            }
+            else
+            {
+                rigid.position = mousePos;
+            }            
+        }
+    }
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rigid.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rigid.isKinematic = false;
+
+        StartCoroutine(ReleaseBall());
+    }
+
+    IEnumerator ReleaseBall()
+    {
+        yield return new WaitForSeconds(releaseBallTime);
+
+        joint.enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+
+        if (nextBall != null)
+        {
+            nextBall.SetActive(true);
+        }
+        else
+        {
+            Enemy.EnemiesAlive = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    public static int EnemiesAlive = 0;
+
+    public float healt = 4f;
+    
+    public GameObject deathEffect;
+    private void Start()
+    {
+        EnemiesAlive++;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > healt)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        GameObject effectClone = Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(effectClone, 2f);
+
+        EnemiesAlive--;
+        if (EnemiesAlive <= 0)
+        {
+            Debug.Log("LEVEL WON");
+        }
+
+        Destroy(gameObject);
+    }
+}
